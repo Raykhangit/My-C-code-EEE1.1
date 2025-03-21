c37a73beb6d0816cd14fd0ac8355461053fea23a
Raykhangit <rayhankhan200512@gmail.com>
2025-03-21 23:48:32 +0600

    Add files via upload

---
+++ Code of all formula.c
@@ -0,0 +1,29 @@
+//ET251081
+#include<stdio.h>
+
+int main(){
+    printf("All Formulas\n");
+    printf("1. Area of Rectangle = a*b\n");
+    printf("2. Area of Triangle = sqrt(s(s-a)*(s-b)*(s-c))\n");
+    printf("3. Area of Triangle = (height*base)/2.0");
+    printf("4. Area of Circle = pi*r*r");
+    printf("5. Radius of Circle = sqrt(a^2+b^2)\n");
+    printf("6. Celcius to Fahrenheit = (9/5)*C+32\n");
+    printf("7. Fahrenheit to Celcius = (5/9)*(F-32)\n");
+    printf("8. Inches to Centimeters = 2.54*I\n");
+    printf("9. Centimeters to Inches = c/2.54\n");
+    printf("10. Square Root of a Number = sqrt(a)\n");
+    printf("11. Square of a Number = a*a\n");
+    printf("12. Greatest of Three Numbers = a>b>c?a:b>c?b:c\n");
+    printf("13. Smalles of Three Numbers = a<b<c?a:b<c?b:c\n");
+    printf("14. Even or Odd = a%2==0?Even:Odd\n");
+    printf("15. Prime or Not = a>1?a%2==0?Not Prime:Prime:Not Prime\n");
+    printf("16. Addition of Two Numbers = a+b\n");
+    printf("17. Subtraction of Two Numbers = a-b\n");
+    printf("18. Multiplication of Two Numbers = a*b\n");
+    printf("19. Division of Two Numbers = a/b\n");
+    printf("20. Modulus of Two Numbers = a%b\n");
+
+
+    return 0;
+}
